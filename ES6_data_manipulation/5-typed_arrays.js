65f9f051334c204c60acf6f0c460b444af7be556
CoeurdArcade <coeurdarcade@gmail.com>
2024-11-19 11:03:35 +0100

    updated script due to checker errors

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
   if (position >= length) {
     throw new Error('Position outside range');
   }
-
-  const finalArray = new Int8Array(length);
-  finalArray[position] = value;
-
-  return finalArray;
+  int8Array[position] = value;
+  return new DataView(buffer);
 }
