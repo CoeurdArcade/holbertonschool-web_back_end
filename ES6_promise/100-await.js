db8f68572052e8d04697015aedd73297f5603e86
CoeurdArcade <coeurdarcade@gmail.com>
2024-11-19 16:22:44 +0100

    script improved due to errors from checker

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -1,12 +1,16 @@
-export default function guardrail(mathFunction) {
-  const queue = [];
-  try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(String(error));
-  } finally {
-    queue.push('Guardrail was processed');
-  }
-  return queue;
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function asyncUploadUser() {
+  Promise.all([
+    await uploadPhoto(),
+    await createUser(),
+  ])
+    .then((results) => ({
+      photo: results[0],
+	    user: results[1],
+    }))
+    .catch((err) => ({
+      photo: null,
+		 user: null,
+    }));
 }
