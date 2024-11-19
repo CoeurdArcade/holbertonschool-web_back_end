6edea4e4a201742c04854a30d77dd403f74b9aec
CoeurdArcade <coeurdarcade@gmail.com>
2024-11-19 11:18:36 +0100

    change script due to checker errors

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -1,15 +1,17 @@
-// A weak map of endpoints and the number of calls made.
 export const weakMap = new WeakMap();
 
-// The maximum number of calls for an endpoint.
-const MAX_ENDPOINT_CALLS = 5;
-
-// Tracks the number of calls made to an API's endpoint.
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  }
+
   const count = weakMap.get(endpoint);
-  weakMap.set(endpoint, count + 1);
-  if (count + 1 > MAX_ENDPOINT_CALLS) {
+
+  if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
+
+  return count;
 }
